32957823bca5ae268234e1d1d9e243a148de0d87
Shaxa8000 <shaxboz.yaxyoyev@bk.ru>
2022-01-18 00:11:05 +0500

    delete, create

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
-import React, {useState} from 'react';
+import React, {useReducer, useState} from 'react';
+
+
+const shaxaReducer = (state, action) => {
+  switch (action.type) {
+    //delete
+    case 'delete': return state.filter(value => value.id !== action.payload.ids);
+
+    //create
+    case 'save': return [...state, {
+      id: state.length + 1,
+      name: action.payload.title
+    }];
+  }
+}
 
 
 const App = () => {
-  const [state, setState] = useState([
-    { id: 1, name: 'Olim' },
-    { id: 2, name: 'Sherzod' },
-    { id: 3, name: 'Sheroz' },
-    { id: 4, name: 'Akmal' },
-  ]);
+  const [name, setName] = useState('');
+
+  const [state, dispatch] = useReducer(shaxaReducer,
+    [
+      { id: 1, name: 'Olim' },
+      { id: 2, name: 'Sherzod' },
+      { id: 3, name: 'Sheroz' },
+      { id: 4, name: 'Akmal' },
+    ],
+  );
+
+
   return (
     <div>
-      <table border='1px' width='600px' style={{borderCollapse:'collapse'}}>
+      <input onChange={(e)=> setName(e.target.value)} type="text" placeholder='enter your name' />
+      <button onClick={()=> dispatch({type: 'save', payload:{title: name}})}>save</button>
+      <table border='1px' width='600px' style={{ borderCollapse: 'collapse' }}>
         <thead>
           <tr>
             <th>Id</th>
@@ -20,10 +42,18 @@ const App = () => {
         </thead>
         <tbody>
           {state.map((value) => (
-            <tr>
+            <tr key={value.id}>
               <td>{value.id}</td>
               <td>{value.name}</td>
-              <td><button>delete</button></td>
+              <td>
+                <button
+                  onClick={() =>
+                    dispatch({ type: 'delete', payload: { ids: value.id } })
+                  }
+                >
+                  delete
+                </button>
+              </td>
             </tr>
           ))}
         </tbody>
